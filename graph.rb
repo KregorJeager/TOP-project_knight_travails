e627067aeb3a110cb83d46c91a190e2052893874
kregor <kregorjeager@gmail.com>
2025-08-30 18:00:05 +0800

    Add method to create graph of all possible moves
    Method will get position of next moves then make
    them a node to add to que and adj of curr node.

--- graph.rb
+++ graph.rb
@@ -5,4 +5,18 @@ class Graph
   def initialize(curr_pos)
     create_graph(curr_pos)
   end
+
+  def create_graph(curr_pos)
+    # return arr of all valid (x,y) postion
+    que = [Gnode.new(curr_pos)]
+    until que.nil?
+      n_moves = next_moves(que[0])
+      n_moves.each do |move|
+        n_node = Gnode.new(move)
+        que[0].adj << n_node
+        que << n_node
+      end
+      que.delete_at(0)
+    end
+  end
 end
