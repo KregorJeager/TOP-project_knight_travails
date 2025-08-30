2c2d3f9e64a7710c121ff8a334677ca925f966d8
kregor <kregorjeager@gmail.com>
2025-08-30 18:35:08 +0800

    Refactor to handle when next move exist
    Will no longer make a new node if the pos(x,y)
    exist in the graph

--- graph.rb
+++ graph.rb
@@ -12,11 +12,13 @@ class Graph
     # return arr of all valid (x,y) postion
     que = [Gnode.new(curr_pos)]
     until que.nil?
-      n_moves = next_moves(que[0])
-      n_moves.each do |move|
-        n_node = Gnode.new(move)
-        que[0].adj << n_node
-        que << n_node
+      # next_move will get arr of all valid (x,y) postion of que
+      next_moves(que[0]).each do |move|
+        # find(move) will return the node given a postion(x,y)
+        connect(que[0], find(move)) if already_exsit?(move)
+        # Create the node give postion(move) then connect it with que[0]
+        # returns the created node
+        que << create_node(que[0], move) unless already_exsit(move)
       end
       que.delete_at(0)
     end
